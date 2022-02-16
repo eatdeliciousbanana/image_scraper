f3620360f59692343d0a000f7826371cafe6b944
eatdeliciousbanana <93957952+eatdeliciousbanana@users.noreply.github.com>
2022-02-17 07:56:44 +0900

    ダウンロード方法を変更

--- scraping.py
+++ scraping.py
@@ -1,22 +1,17 @@
-# モジュール
-import requests
-# from bs4 import BeautifulSoup
-from selenium import webdriver
+import os
+import urllib.parse
 from time import sleep
 import re
-import os
-import shutil
-import urllib.parse
 import pathlib
+import requests
+from selenium import webdriver
+from selenium.webdriver.chrome import service as fs
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome import service as fs
 
 
 # 設定
-SEARCH_WORD = ""  # 検索ワード
-IMG_NUM = 0  # 画像枚数
-PATH = r""  # 保存先パス
+PATH = os.path.join(os.environ["USERPROFILE"], "Downloads")  # 保存先パス
 SLEEP_BETWEEN_INTERACTIONS = 2  # クリックなどの動作後の待ち時間（秒）
 
 
@@ -31,12 +26,22 @@ options.add_experimental_option(
 
 # main関数
 def main():
+    print("検索ワードを入力してください")
+    print("例: ねこ　かわいい　イラスト")
+    search_word = input("検索ワード: ")
+    print()
+
+    print("ダウンロードする枚数を入力してください")
+    print("例: 10")
+    img_num = int(input("枚数: "))
+    print()
+
     # ブラウザ起動
     driver = webdriver.Chrome(service=chrome_service, options=options)
 
     # Google画像検索でキーワードを検索
     driver.get(
-        "https://www.google.co.jp/search?q={}&hl=ja&tbm=isch".format(urllib.parse.quote(SEARCH_WORD)))
+        "https://www.google.co.jp/search?q={}&hl=ja&tbm=isch".format(urllib.parse.quote(search_word)))
 
     # ページ上の画像のサムネイルを読み込み
     print("ページ読み込み: 開始-------------------------------------------------------------------------------")
@@ -46,7 +51,7 @@ def main():
             By.XPATH, "//img[@class='rg_i Q4LuWd']")
         count = len(thumbnails)
         print("画像候補数: "+str(count))
-        if(count > IMG_NUM*2 or count == temp):
+        if(count > img_num*2 or count == temp):
             break
         temp = count
         driver.execute_script(
@@ -59,14 +64,14 @@ def main():
     img_urls = []
     count = 1
     for thumbnail in thumbnails:
-        if count > IMG_NUM:
+        if count > img_num:
             break
         driver.execute_script('arguments[0].click();', thumbnail)
         sleep(SLEEP_BETWEEN_INTERACTIONS)
         candidates = driver.find_elements(By.CLASS_NAME, "n3VNCb")
         for candidate in candidates:
             img_url = candidate.get_attribute("src")
-            if re.search("^https", img_url) and re.search(".jpg$", img_url):
+            if re.search("^https", img_url) and re.search("jpg$", img_url):
                 img_urls.append(img_url)
                 print("画像"+str(count)+": "+img_url)
                 count += 1
@@ -78,14 +83,14 @@ def main():
 
     # 画像をダウンロードして保存
     print("画像ダウンロード: 開始-----------------------------------------------------------------------------")
-    word = SEARCH_WORD.replace("　", " ").replace(" ", "_")
+    word = search_word.replace("　", " ").replace(" ", "_")
     image_path = os.path.join(PATH, word)
     pathlib.Path(image_path).mkdir()
     for i, img_url in enumerate(img_urls, 1):
-        res = requests.get(img_url, stream=True)
+        res = requests.get(img_url)
         filename = word+"_"+str(i)+".jpg"
         with open(os.path.join(image_path, filename), "wb") as file:
-            shutil.copyfileobj(res.raw, file)
+            file.write(res.content)
         print("画像"+str(i)+": "+filename)
         sleep(SLEEP_BETWEEN_INTERACTIONS)
     print("画像ダウンロード: 完了-----------------------------------------------------------------------------")
